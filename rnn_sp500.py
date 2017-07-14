b4dd682f6ccbfe003b622f1db980c6712b9eef9d
Da Xu <exutechnology@gmail.com>
2017-07-14 16:12:27 -0400

    working version

--- rnn_sp500.py
+++ rnn_sp500.py
@@ -24,7 +24,7 @@ df.loc[:,2] *= 0.001
 df.loc[:,3] *= 0.001
 df.loc[:,4] *= 0.001
 df.loc[:,5] *= 0.001
-df.loc[:,6] *= 0.000000001
+df.loc[:,6] *= 0.00000000001
 
 
 data = df.values
@@ -33,7 +33,10 @@ h, w = data.shape
 
 n_steps = 50
 n_echo = 5
-batch_size = 100
+batch_size = 40
+epochs = 25
+
+
 
 s=(h-n_echo-n_steps+1,50,6)
 hist_s = np.zeros([h-n_echo-n_steps+1,50,6])
@@ -58,14 +61,13 @@ is 50.
 
 # Parameters
 learning_rate = 0.001
-n_test_samples = 100
+n_test_samples = 1000
 training_iters = n_samples - n_test_samples
-batch_size = 128
+
 display_step = 10
 
 # Network Parameters
 n_input = w # MNIST data input (img shape: 28*28)
-#n_steps = 50 # timesteps
 n_hidden = 128 # hidden layer num of features
 n_outputs = 6 # open high low close  volume adj close
 
@@ -104,6 +106,8 @@ pred = RNN(x, weights, biases)
 
 # Define loss and optimizer
 cost = tf.reduce_mean(tf.squared_difference(pred, y))
+cost_prediction = tf.reduce_mean(tf.squared_difference(pred[4], y[4]))
+
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Evaluate model
@@ -119,26 +123,37 @@ init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
     step = 1
-    # Keep training until reach max iterations
-    while step * batch_size < training_iters:
-        batch_x = hist_prices[(step-1)*batch_size:(step*batch_size)][:][:]
-        batch_y = toPredict[(step-1)*batch_size:(step*batch_size)][:]
+    # Keep training until reach max iterations  
+    
+    for i in range(epochs):
+        step = 1
+        print ("Epoch ", i)
+        while step * batch_size < training_iters:
+            batch_x = hist_prices[(step-1)*batch_size:(step*batch_size)][:][:]
+            batch_y = toPredict[(step-1)*batch_size:(step*batch_size)][:]
         
-        # Run optimization op (backprop)
-        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y})
-        if step % display_step == 0:
-            # Calculate batch accuracy
-            acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y})
-            # Calculate batch loss
-            loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y})
-            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
-                  "{:.6f}".format(loss) + ", Training Accuracy= " + \
-                  "{:.5f}".format(acc))
-        step += 1
+            # Run optimization op (backprop)
+            sess.run(optimizer, feed_dict={x: batch_x.eval(), y: batch_y.eval()})
+            if step % display_step == 0:
+                # Calculate batch accuracy
+                acc = sess.run(accuracy, feed_dict={x: batch_x.eval(), y: batch_y.eval()})
+                # Calculate batch loss
+                loss = sess.run(cost, feed_dict={x: batch_x.eval(), y: batch_y.eval()})
+                print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+                  "{:.6f}".format(loss))
+            step += 1
+        
     print("Optimization Finished!")
 
     # Calculate accuracy for test prices 
     test_hist_prices = hist_prices[(n_samples-n_test_samples):n_samples][:][:]
     test_toPredict = toPredict[(n_samples-n_test_samples):n_samples][:]
-    print("Testing Accuracy:", \
-sess.run(accuracy, feed_dict={x: test_hist_prices, y: test_toPredict}))
+    testLoss = sess.run(cost_prediction, feed_dict={x: test_hist_prices.eval(), y: test_toPredict.eval()})
+    print("Testing loss:", testLoss)
+   
+    print(test_toPredict.eval())
+    ss = 0.0    
+    for i in range(n_test_samples-n_echo):
+        ss = ss+ (test_toPredict.eval()[i][4]-test_toPredict.eval()[i+n_echo][4])**2
+    print("the variance of outputs ",  ss/(n_test_samples-n_echo))
+    print("prediction of ", n_echo, "days")
