284ed7e43dda87a07f6ff74498f91617cf2d0884
Da Xu <Da.Xu@8451.com>
2018-03-26 17:22:49 -0400

    add two modified models for research

---
+++ translate/modified/__init__.py
@@ -0,0 +1,19 @@
+"""
+@@MaskBasicLSTMCell
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# go/tf-wildcard-import
+# pylint: disable=wildcard-import
+from modified.rnn_cell_impl import *
+# pylint: enable=wildcard-import
+from tensorflow.python.util.all_util import remove_undocumented
+
+_allowed_symbols = []
+
+remove_undocumented(__name__, _allowed_symbols)
+
+
